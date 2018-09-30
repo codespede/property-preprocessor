64feed81e1b529aef1b8c52bbebe2e9aa43e7110
Mahesh S Warrier <maheshs60@gmail.com>
2018-09-30 20:01:40 +0530

    Create PreProcessorTrait.php

---
+++ PreProcessorTrait.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package   property-preprocessor
+ * @author    Mahesh S Warrier <maheshs60@gmail.com>
+ * @copyright Copyright &copy; Mahesh S Warrier, 2018
+ * @version   1.0.0
+ */
+/**
+ * Trait PreProcessorTrait
+ * @package codespede\yii2-formatter
+ */
+trait PreProcessorTrait{
+    private $methods;
+    public function __get($name){
+        $this->methods = get_class_methods($this);
+        foreach($this->methods as $method){
+            $key = lcfirst(str_replace("get", "", $method));
+            if(strpos($name, $key) !== FALSE){ 
+                $actualValue = $this->{lcfirst(str_replace($key, "", $name))};
+                return $this->{"get".ucfirst($key)}($actualValue);
+            }
+        }
+        return parent::__get($name);
+    }
+}
